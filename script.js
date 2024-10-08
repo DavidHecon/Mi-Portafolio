99fb77bcfe349d3ce6729888cb24493dc0d1d3fd
David Hernández Contreras <100537779+DavidHecon@users.noreply.github.com>
2024-10-08 12:48:35 -0600

    first comment

---
+++ script.js
@@ -0,0 +1,4 @@
+//La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
+function redirigir(url) {
+    window.location.href = url;
+}
