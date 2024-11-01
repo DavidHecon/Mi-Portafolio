ae178786346d2cd982c2625c92c2be2aa7484541
David Hernández Contreras <100537779+DavidHecon@users.noreply.github.com>
2024-11-01 08:11:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //La validación del formulário con javascript es considerada un desafío extra, no es obligatório realizar esta validación para realizar su entrega
 function redirigir(url) {
-    window.location.href = url;
+    window.open(url, '_blank');   
 }
